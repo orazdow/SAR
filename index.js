527db7cf39da427384dab5a83f408e26a8013327
orazdow <ollierazdow@hotmail.com>
2019-10-30 22:41:10 -0700

    ok

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ class Main extends Component{
 		// window.statuses = statuses;
 		window.tree = sar_tree;
 		this.nodeMouseOver = this.nodeMouseOver.bind(this);
+		this.nodeMouseClick = this.nodeMouseClick.bind(this);
+		this.nodeMouseOut = this.nodeMouseOut.bind(this);
 		this.disp;
+		this.rect;
+		this.selected = null;
 	}
 
 	componentDidMount(){
@@ -24,6 +28,17 @@ class Main extends Component{
 		// document.querySelector('body').style.backgroundColor = "#333333";
 		document.querySelector('body').style.backgroundColor = "#dddddd";
 		this.disp = document.querySelector("#readContent");
+		let svg = document.querySelector(".rd3t-tree-container svg g");
+        this.rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+        this.rect.setAttributeNS(null, 'x', -25);
+        this.rect.setAttributeNS(null, 'y', -25);
+        this.rect.setAttributeNS(null, 'height', '50');
+        this.rect.setAttributeNS(null, 'width', '50');
+        this.rect.setAttributeNS(null, 'fill', '#FF3333');
+        this.rect.setAttributeNS(null, 'stroke', '#FF3333');
+        this.rect.setAttributeNS(null, 'fill-opacity', '0.0');
+        this.rect.setAttributeNS(null, 'stroke-opacity', '0.0');
+        svg.appendChild(this.rect);
 
 	}
 
@@ -31,26 +46,50 @@ class Main extends Component{
 		let content = event.status.content;
 		// console.log(content);
 		this.disp.innerHTML = content;
+
+	}
+
+	nodeMouseOut(event){
+		if(!this.selected){
+
+		}else{
+			this.disp.innerHTML = this.selected.status.content;
+		}
+	}
+
+	nodeMouseClick(event){
+		//console.log(event);
+		this.selected = event;
+        this.rect.setAttributeNS(null, 'x', event.x-25);
+        this.rect.setAttributeNS(null, 'y', event.y-25);
+        this.rect.setAttributeNS(null, 'stroke-opacity', '1.0');
 	}
 
 	render(){
 
 		return(
 
-			<div id="maindiv" >
+			<div id="maindiv" style={{'display' : 'inline-block'}}>
 
 			{/*<ReactJson src={sar_tree} collapsed={true} theme="monokai" />*/}
 
 
 			<div className="treeDiv" style={{width: '60em', height: '150em', float: 'left'}}>
-			<Tree data={sar_tree} orientation="vertical" zoom={0.6} translate={{x:400, y:20}} onMouseOver={this.nodeMouseOver}/>
+			<Tree data={sar_tree} 
+			orientation="vertical" 
+			collapsible={false}
+			zoom={0.6} 
+			translate={{x:400, y:20}} 
+			onMouseOver={this.nodeMouseOver}
+			onMouseOut={this.nodeMouseOut}
+			onClick={this.nodeMouseClick}
+			/>
 			</div>
 
 
-			<div className="readout" >
-			<p id="readContent"></p>
+			<div className="readout" style={{ 'width' : '520px', 'float' : 'right' }}>
+			<p id="readContent" style={{'width':'100%'}}></p>
 			</div>
-
 			</div>
 
 		);
